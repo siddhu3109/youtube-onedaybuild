55870b8a32ad98d74126798ae532b79d918118e6
Shekhar Pal <palshekhar197@gmail.com>
2020-12-16 13:46:37 +0530

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Shekhar Pal on 16/12/20.
+//  Copyright © 2020 siddharth pal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ youtube-onedaybuildTests/youtube_onedaybuildTests.swift
@@ -0,0 +1,34 @@
+//
+//  youtube_onedaybuildTests.swift
+//  youtube-onedaybuildTests
+//
+//  Created by Shekhar Pal on 16/12/20.
+//  Copyright © 2020 siddharth pal. All rights reserved.
+//
+
+import XCTest
+@testable import youtube_onedaybuild
+
+class youtube_onedaybuildTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
